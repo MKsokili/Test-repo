015928539d41ed0d634d8f7c2b2a1572d9f490fc
MKsokili <123400707+MKsokili@users.noreply.github.com>
2023-01-23 21:31:44 +0100

    Add files via upload

---
+++ pilote.sql
@@ -0,0 +1,18 @@
+CREATE TABLE Pilote
+(
+	brevet char(5),
+	prenom char(10),
+	nom char(10),
+	surnom char(10),
+	nbhvol INTEGER,
+	compa char(5)
+);
+
+
+
+INSERT INTO Pilote VALUES('PL-1','Gratien','viel','dba', 100 ,'AF');
+INSERT INTO Pilote VALUES('PL-2','Didier','donsez','smith', 400 ,'AF');
+INSERT INTO Pilote VALUES('PL-3','richard','GrIN','Faucon', 900 ,'SING');
+INSERT INTO Pilote VALUES('PL-4','Placide','Fresnais','cool', 600 ,'AF');
+INSERT INTO Pilote VALUES('PL-5','Daniel','vielle','jone', 300 ,'SING');
+INSERT INTO Pilote VALUES('PL-6','Francoise','tort','normaleSup', 450 , 'CAST');
